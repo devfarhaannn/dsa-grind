b4c7fa5c626bd51b9649c89cde706eec9f1be7d0
devfarhaannn <bhattfarhan1804@gmail.com>
2026-05-18 12:29:18 +0530

    multiple recursion problems

---
+++ 06-recursion-basic/06-multiple-recursion-problems/01_fibonacci_series.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacciSeries(int n)
+{
+    if (n <= 1)
+        return n;
+    int last = fibonacciSeries(n - 1);
+    int slast = fibonacciSeries(n - 2);
+    return last + slast;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // cout << "fibonacci number: " << fibonacciSeries(n) << " ";
+    
+    cout << "fibonacci series is: " ;
+    for (int i = 0; i < n; i++)
+    {
+        cout << fibonacciSeries(i) << " ";
+    }
+    return 0;
+}
