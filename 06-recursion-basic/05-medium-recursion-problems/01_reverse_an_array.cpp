12f79c0fa73c9ab84355c5c7cc2612b09781270e
devfarhaannn <bhattfarhan1804@gmail.com>
2026-05-18 11:47:21 +0530

    medium recursion problems

---
+++ 06-recursion-basic/05-medium-recursion-problems/01_reverse_an_array.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverse1(vector<int> &arr, int l, int r)
+{
+
+    if (l >= r)
+    {
+        return;
+    }
+
+    swap(arr[l], arr[r]);
+    reverse1(arr, l + 1, r - 1);
+}
+
+void reverseWithOneVar(int i, vector<int> &arr, int n)
+{
+    if (i >= (n / 2))
+    {
+        return;
+    }
+
+    swap(arr[i], arr[n - i - 1]);
+    reverseWithOneVar(i + 1, arr, n);
+}
+
+int main()
+{
+
+    // int n = 5;
+    // int arr[] = {1, 2, 3, 4, 5};
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    reverse1(arr, 0, n - 1);
+    // reverse1(arr, 0, n-1);
+
+    reverseWithOneVar(0, arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
